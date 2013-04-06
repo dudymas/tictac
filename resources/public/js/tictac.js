854b0f8eefab8533a9f63532377fa0fb25fc37ea
Jeremy White <dudymas@gmail.com>
2013-04-06 16:10:25 -0400

    completed angular models

--- resources/public/js/tictac.js
+++ resources/public/js/tictac.js
@@ -1,7 +1,7 @@
 
 angular.module("tictac", [])
 	.factory("Player", function (){
-		var player = {piece : "O"};
+		var player = {piece : "O", type: "human"};
 		var Player = function Player () {};
 		Player.prototype.getPiece = function() {
 			return player.piece;//make a ui call later if this isn't set yet
@@ -10,6 +10,32 @@ angular.module("tictac", [])
 			return player;
 		};
 		return new Player();
+	})
+	.factory("Computer", function() {
+		var computer = {piece : "X", type: "computer"};
+		var Computer = function Computer () {};
+		Computer.prototype.getData = function() {
+			return computer;
+		};
+		return new Computer();
+	})
+	.factory("Game", function(Player,Computer,Board) {
+		var game = {
+			"last-turn" : {player: Player   .getData()},
+			board       : 		   Board    .getData(),
+			turn        : {player: Computer .getData()}
+		};
+		game.players = [game.turn.player, game["last-turn"].player];
+		var Game = function Game () {}
+		Game.prototype.getData = function() {
+			return game;
+		};
+		Game.prototype.move = function(player, position) {
+			if (game["last-turn"].player === player) {
+				game["last-turn"].position = position;
+			}
+		};
+		return new Game();
 	})
 	.factory("Board", function() {
 		var board = [
@@ -28,7 +54,7 @@ function boardCtrl ($scope, Board) {
 	$scope.board = Board.getData();
 }
 
-function rowCtrl ($scope, Player) {
+function rowCtrl ($scope, Player, Game) {
 	$scope.setPiece = function(pos) {
 		var piece = $scope.row[pos];
 		if (piece == null)
@@ -38,12 +64,15 @@ function rowCtrl ($scope, Player) {
 		else
 			piece = null;
 		$scope.row[pos] = piece;
+		Game.move(Player.getData(), [$scope.$index, pos]);
 	};
 }
 
-function debugCtrl ($scope, Board, Player) {
+function debugCtrl ($scope, Player, Computer, Board, Game) {
 	//show info about our state
 	$scope.data = {};
 	$scope.data.board = Board.getData();
 	$scope.data.player = Player.getData();
+	$scope.data.computer = Computer.getData();
+	$scope.data.game = Game.getData();
 }
