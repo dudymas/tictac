72917dccfd66185d24259cf877dcbb929a195d92
Jeremy White <dudymas@gmail.com>
2013-04-06 16:38:39 -0400

    computer interface fleshed out

--- resources/public/js/tictac.js
+++ resources/public/js/tictac.js
@@ -12,20 +12,28 @@ angular.module("tictac", [])
 		return new Player();
 	})
 	.factory("Computer", function() {
-		var computer = {piece : "X", type: "computer"};
+		var computer = {piece : "X", type: "computer", "is-on": true};
 		var Computer = function Computer () {};
 		Computer.prototype.getData = function() {
 			return computer;
 		};
+		Computer.prototype.requestMove = function(game) {
+			var row = Math.floor(Math.random()*3);
+			var col = Math.floor(Math.random()*3);
+			return [row,col];
+		};
 		return new Computer();
 	})
 	.factory("Game", function(Player,Computer,Board) {
+		var humanPlayer = Player.getData();
+		var computerPlayer = Computer.getData();
+
 		var game = {
-			"last-turn" : {player: Player   .getData()},
+			"last-turn" : {player: humanPlayer},
 			board       : 		   Board    .getData(),
-			turn        : {player: Computer .getData()}
+			turn        : {player: computerPlayer},
+			players		: [humanPlayer, computerPlayer]
 		};
-		game.players = [game.turn.player, game["last-turn"].player];
 		var Game = function Game () {}
 		Game.prototype.getData = function() {
 			return game;
@@ -33,6 +41,10 @@ angular.module("tictac", [])
 		Game.prototype.move = function(player, position) {
 			if (game["last-turn"].player === player) {
 				game["last-turn"].position = position;
+				if (computerPlayer["is-on"]) {
+					var move = Computer.requestMove(game);
+					game.board[move[0]][move[1]] = computerPlayer.piece;
+				}
 			}
 		};
 		return new Game();
@@ -66,6 +78,10 @@ function rowCtrl ($scope, Player, Game) {
 		$scope.row[pos] = piece;
 		Game.move(Player.getData(), [$scope.$index, pos]);
 	};
+}
+
+function computerCtrl ($scope, Computer) {
+	$scope.computer = Computer.getData();
 }
 
 function debugCtrl ($scope, Player, Computer, Board, Game) {
