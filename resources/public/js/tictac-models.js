c37a014cd81a66837e57f94d30affb0b39f4a211
Jeremy White <dudymas@gmail.com>
2013-06-02 23:32:50 -0400

    tictac-models tests fleshed out
    some room for improvement, but shouldn't be too bad

--- resources/public/js/tictac-models.js
+++ resources/public/js/tictac-models.js
@@ -1,13 +1,43 @@
 angular.module('tictac-models',[])
 	.factory("Player", function() {
 		var Player = function Player () {};
+		Player.create = function(piece, type) {
+			var result = new Player();
+			result["game-piece"] = piece;
+			result.type = type;
+			return result;
+		};
 		return Player;
 	})
-	.factory("Board", function() {
+	.factory("Row", function() {
+		var Row = function Row () {};
+		Row.create = function(rowLength) {
+			var result = [];
+			while(result.length < rowLength)
+				result.push(null);
+			return result;
+		};
+		return Row;
+	})
+	.factory("Board", function(Row) {
 		var Board = function Board () {};
+		Board.create = function(rowCount, rowLength) {
+			var result = [];
+			while(result.length < rowCount)
+				result.push(Row.create(rowLength));
+			return result;
+		};
 		return Board;
 	})
 	.factory("Game", function() {
-		var Game = function Game (players, board) {};
+		var Game = function Game () {};
+		Game.create = function(players, board) {
+			var result = new Game();
+			result.players = [];
+			for (var playerIdx = 0; playerIdx < players.length; playerIdx++)
+				result.players.push(players[playerIdx]);
+			result.board = board;
+			return result;
+		};
 		return Game;
 	});
--- resources/test/js/tictac-modelsTests.js
+++ resources/test/js/tictac-modelsTests.js
@@ -3,9 +3,29 @@ describe('tictac-models', function(){
 	beforeEach(function() {
 		module('tictac-models');
 	});
+
 	it('has player', function() {
 		inject(function(Player) {
 			expect(Player).any;
+		});
+	});
+
+	describe('player', function () {
+		var player;
+		var piece = "O";
+		var type = "test";
+
+		beforeEach(function() {
+			inject(function(Player) {
+				player = Player.create(piece, type);
+			});
+		});
+		
+		it('should have a piece', function () {
+			expect(player["game-piece"]).toBe(piece);
+		});
+		it('should have a type', function () {
+			expect(player.type).toBe(type);
 		});
 	});
 	it('has board', function() {
@@ -13,9 +33,54 @@ describe('tictac-models', function(){
 			expect(Board).any;
 		});
 	});
+	describe('board', function () {
+		var board;
+		var rowCount = 3;
+		var rowLength = 3;
+
+		beforeEach(function(){
+			inject(function(Board) {
+				board = Board.create(rowCount, rowLength);
+			});
+		});
+
+		it('should be an array', function () {
+			expect(board.length).toBeDefined();
+		});
+		it('should contain the specified number of rows', function () {
+			expect(board.length).toBe(rowCount);
+		});
+		it('should have rows with the specified length', function () {
+			while(board.length)
+				expect(board.pop().length).toBe(rowLength);
+		});
+	});
 	it('has game', function() {
 		inject(function(Game){
 			expect(Game).any;
+		});
+	});
+	describe('Game', function () {
+		var game, players, board;
+		beforeEach(function() {
+			inject(function(Game){
+				players = [{}, {}];
+				board = [ [] ];
+				game = Game.create(players, board);
+			});
+		});
+		it('should have players', function () {
+			expect(game.players).toBeDefined();
+		});
+		it('should have the players that were specified', function () {
+			while (game.players.length)
+				expect(players).toContain(game.players.pop());
+		});
+		it('should have a board', function () {
+			expect(game.board).toBeDefined();
+		});
+		it('should contain the board that was specified', function () {
+			expect(game.board).toBe(board);
 		});
 	});
 });
