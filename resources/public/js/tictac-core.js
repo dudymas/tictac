3244f989d28417102ee5bf4cd8329688c4d24234
Jeremy White <dudymas@gmail.com>
2013-06-04 16:39:37 -0400

    added more detailed tests to tictac js core

--- resources/public/js/tictac-core.js
+++ resources/public/js/tictac-core.js
@@ -1,5 +1,5 @@
-angular.module('tictac-core',[])
-	.value('Player1',{})
-	.value('Player2',{})
-	.value('Board',{})
-	.value('Game',{});
+angular.module('tictac-core',['tictac-models'])
+	.factory('Player1', function(Player) { return Player.create("O", "human"); } )
+	.factory('Player2',function(Player) { return Player.create("O", "human"); } )
+	.value('CurrentBoard',{})
+	.factory('CurrentGame',function(Game, Player1, Player2) {return Game.create([Player1, Player2]); });
--- resources/test/js/tictac-coreTests.js
+++ resources/test/js/tictac-coreTests.js
@@ -8,19 +8,53 @@ describe('tictac-core', function () {
 			expect(Player1).any;
 		});
 	});
+	describe('Player1', function(){
+		var player;
+		beforeEach(inject(function(Player1) {
+			player = Player1;
+		}));
+
+		it('should be a Player',function() {
+			expect(player.type).toBeDefined();
+			expect(player["game-piece"]).toBeDefined();
+		});
+		it('should be the first player', function() {
+			inject(function(CurrentGame) {
+				expect(CurrentGame.players).toBeDefined();
+				//expect(Game.players[0]).toBe(player);
+			});
+		});
+	});
 	it('has Player2', function () {
 		inject(function(Player2){
 			expect(Player2).any;
 		});
 	});
-	it('has Board', function () {
-		inject(function(Board){
-			expect(Board).any;
+	describe('Player2', function(){
+		var player;
+		beforeEach(inject(function(Player2) {
+			player = Player2;
+		}));
+
+		it('should be a Player',function() {
+			expect(player.type).toBeDefined();
+			expect(player["game-piece"]).toBeDefined();
+		});
+		it('should be the second player', function() {
+			inject(function(CurrentGame) {
+				expect(CurrentGame.players).toBeDefined();
+				expect(CurrentGame.players[1]).toBe(player);
+			});
 		});
 	});
-	it('has Game', function () {
-		inject(function(Game){
-			expect(Game).any;
+	it('has CurrentBoard', function () {
+		inject(function(CurrentBoard){
+			expect(CurrentBoard).any;
+		});
+	});
+	it('has CurrentGame', function () {
+		inject(function(CurrentGame){
+			expect(CurrentGame).any;
 		});
 	});
 });
